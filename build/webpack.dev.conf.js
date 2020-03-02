1ef07534d0aa5ab7eef2fa6d077272653de89095
chengbotao <t221007s@163.com>
2020-03-02 23:41:32 +0800

    [build]: 增加 webpack.NoEmitOnErrorsPlugin 屏蔽错误

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const merge = require("webpack-merge");
+const webpack = require("webpack");
 const webpackBaseConf = require("./webpack.base.conf");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -7,8 +8,10 @@ module.exports = merge(webpackBaseConf, {
   plugins: [
     // HTML 模板
     new htmlWebpackPlugin({
-      template: path.resolve(__dirname, "../index.html"),
-      bundleName: "./src/assets/dll/vendor.dll.js"
-    })
+      template: path.resolve(__dirname, "../index.html")
+    }),
+
+    // 在编译出现错误时,代码将不会退出。
+    new webpack.NoEmitOnErrorsPlugin()
   ]
 })
